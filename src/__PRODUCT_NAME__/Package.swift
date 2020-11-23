51e132432ec6def8731256de05a750a5ef694a31
Paul Calnan <pwc3@users.noreply.github.com>
2020-11-23 12:34:29 -0500

    Cocoapods to spm (#1)
    * Remove CocoaPods, add Package.swift
    
    * Added content to LaunchScreen storyboard
    
    * Fix script to remove reference to pod install, xcworkspace
    
    * Include __YEAR__ variable in Package.swift
    
    * Update gitignore

--- src/__PRODUCT_NAME__/Package.swift
+++ src/__PRODUCT_NAME__/Package.swift
@@ -1,6 +1,6 @@
 //
-//  __PRODUCT_NAME__Tests.swift
-//  __PRODUCT_NAME__Tests
+//  Package.swift
+//  __PRODUCT_NAME__
 //
 //  Copyright (c) __YEAR__ __ORGANIZATION_NAME__
 //
@@ -23,29 +23,23 @@
 //  DEALINGS IN THE SOFTWARE.
 //
 
-import XCTest
-@testable import __PRODUCT_NAME__
+import PackageDescription
 
-class __PRODUCT_NAME__Tests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        print(__PRODUCT_NAME__.text)
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-}
+let package = Package(
+    name: "__PRODUCT_NAME__",
+    products: [
+        .library(
+            name: "__PRODUCT_NAME__",
+            targets: ["__PRODUCT_NAME__"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "__PRODUCT_NAME__",
+            dependencies: []),
+        .testTarget(
+            name: "__PRODUCT_NAME__Tests",
+            dependencies: ["__PRODUCT_NAME__"]),
+    ]
+)
